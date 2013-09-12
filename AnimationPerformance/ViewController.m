156b5fb0e93627f4d3d23de130d78a5d74f352aa
Mark Pospesel <mpospese@gmail.com>
2013-09-11 23:24:01 -0600

    re-enable rasterization
    Ok, that didn't help (and seems to have hurt) performance, so re-enable
    rasterization.

--- AnimationPerformance/ViewController.m
+++ AnimationPerformance/ViewController.m
@@ -63,8 +63,7 @@
 {
     UIView *box = [self makeBoxWithFrame:CGRectMake(0, 0, BOX_SIZE, BOX_SIZE)];
     // turn on rasterizing to flatten the box view's sub-tree for better performance
-    // Now that we're setting shadowPath, try disabling rasterization
-    //box.layer.shouldRasterize = YES;
+    box.layer.shouldRasterize = YES;
     
     [self.view addSubview:box];
     [self.boxes addObject:box];
